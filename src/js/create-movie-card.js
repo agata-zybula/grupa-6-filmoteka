25fc98274a2985f995164be7f395c6d914a5e155
Katarzyna Cependa <kasia.cependa@gmail.com>
2023-05-23 18:45:54 +0200

    Adding functionality to display posters

--- src/js/create-movie-card.js
+++ src/js/create-movie-card.js
@@ -1,7 +1,7 @@
 export const createMovieCard = movies => {
   return movies.results
     .map(movie => {
-      return `<div id="card" class="card"><img class="card__poster" src=${movie.poster_path} alt=${movie.title}></a>
+      return `<div id="card" class="card"><img class="card__poster" src='https://image.tmdb.org/t/p/w220_and_h330_face${movie.poster_path}' alt=${movie.title}></a>
     <div class="card__info">
       <div class="card__quick-info">
         <div class="card__movie-title">${movie.title}</div>
