e95a6dcfcc14d58108ed030e37ac34195b1f2408
KonradSadowski <darnokowsky@gmail.com>
2023-05-27 19:52:28 +0200

    pagination fo popular and search

--- src/js/popular.js
+++ src/js/popular.js
@@ -1,11 +1,10 @@
-// import { createMovieCard } from './create-movie-card';
-import { genreList, getGenres } from './fetch-genres';
-import axios from 'axios';
+// // import { createMovieCard } from './create-movie-card';
+// import { genreList, getGenres } from './fetch-genres';
+// import axios from 'axios';
 
-const API_key = 'dbea77d3eb5b3622b027f73f6a5032fe';
-const galleryEl = document.querySelector('.cards-wrapper');
+// const API_key = 'dbea77d3eb5b3622b027f73f6a5032fe';
 
-// Pagination elements
+// // Pagination elements
 // let currentPage = 1;
 // let firstPage = 1;
 // let lastPage = 20;
@@ -23,55 +22,56 @@ const galleryEl = document.querySelector('.cards-wrapper');
 // const dots1El = document.getElementById('dots1');
 // const dots2El = document.getElementById('dots2');
 // const galleryEl = document.querySelector('.cards-wrapper');
-// ------------------------------------------------------------------------------------------
+// // ------------------------------------------------------------------------------------------
 
-const getTrending = async () => {
-  try {
-    const searchTrendingAPI_URL = 'https://api.themoviedb.org/3/trending/movie/day';
-    const result = await axios.get(
-      `${searchTrendingAPI_URL}?api_key=${API_key}& page=1&language=en-US`,
-    );
+// const getTrending = async () => {
+//   try {
+//     const searchTrendingAPI_URL = 'https://api.themoviedb.org/3/trending/movie/day';
+//     const result = await axios.get(
+//       `${searchTrendingAPI_URL}?api_key=${API_key}&page=${currentPage}&language=en-US`,
+//     );
 
-    const movies = result.data.results;
-    await getGenres();
+//     const movies = result.data.results;
+//     await getGenres();
 
-    return movies;
-  } catch (error) {
-    console.error(error);
-  }
-};
+//     return movies;
+//   } catch (error) {
+//     console.error(error);
+//   }
+// };
 
-const createMovieCard = movies => {
-  return movies
-    .map(movie => {
-      const genreNames = movie.genre_ids
-        .slice(0, 3)
-        .map(genreId => genreList[genreId])
-        .join(', ');
-      return `<div id="card" class="card"><img class="card__poster" src='https://image.tmdb.org/t/p/w220_and_h330_face${
-        movie.poster_path
-      }' alt='Poster of ${movie.title} movie'></a>
-    <div class="card__info">
-      <div class="card__quick-info">
-        <div class="card__movie-title">${movie.title}</div>
-        <div class="card__movie-genre">${genreNames}</div>
-        <div class="card__movie-release">${movie.release_date.slice(0, 4)}</div>
-      </div>
-      <div class="card__movie-rating">${Math.round(movie.vote_average * 10) / 10}</div>
-    </div>
-  </div>`;
-    })
-    .join('');
-};
+// const createMovieCard = movies => {
+//   return movies
+//     .map(movie => {
+//       const genreNames = movie.genre_ids
+//         .slice(0, 3)
+//         .map(genreId => genreList[genreId])
+//         .join(', ');
+//       return `<div id="card" class="card"><img class="card__poster" src='https://image.tmdb.org/t/p/w220_and_h330_face${
+//         movie.poster_path
+//       }' alt='Poster of ${movie.title} movie'></a>
+//     <div class="card__info">
+//       <div class="card__quick-info">
+//         <div class="card__movie-title">${movie.title}</div>
+//         <div class="card__movie-genre">${genreNames}</div>
+//         <div class="card__movie-release">${movie.release_date.slice(0, 4)}</div>
+//       </div>
+//       <div class="card__movie-rating">${Math.round(movie.vote_average * 10) / 10}</div>
+//     </div>
+//   </div>`;
+//     })
+//     .join('');
+// };
 
-getTrending().then(movies => {
-  galleryEl.innerHTML = createMovieCard(movies);
-});
+// getTrending().then(movies => {
+//   galleryEl.innerHTML = createMovieCard(movies);
+// });
 
-// Pagination
+// // Pagination
 // const updatePage = () => {
-//   getMovies();
-//   createGallery();
+//   getTrending().then(movies => {
+//     galleryEl.innerHTML = createMovieCard(movies);
+//   });
 //   if (currentPage === 1) {
 //     currentPageButton.innerHTML = currentPage;
 //     arrowLeftButton.style.visibility = 'hidden';
@@ -179,62 +179,4 @@ getTrending().then(movies => {
 // currentPageAddTwoButton.addEventListener('click', e => {
 //   e.preventDefault();
 //   handlePageUpdate(2);
-// });
-
-// firstPageButton.addEventListener('click', e => {
-//   e.preventDefault();
-//   currentPage = firstPage;
-//   currentPageButton.innerHTML = currentPage;
-//   if (currentPage <= 3) {
-//     currentPageButton.innerHTML = currentPage;
-//     currentPageAddOneButton.innerHTML = '';
-//     currentPageAddTwoButton.innerHTML = '';
-//     currentPageMinusOneButton.innerHTML = '';
-//     currentPageMinusTwoButton.innerHTML = '';
-//     dots1El.innerHTML = '';
-//     dots2El.innerHTML = '';
-//     firstPageButton.innerHTML = '';
-//     lastPageButton.innerHTML = '';
-//   } else {
-//     currentPageButton.innerHTML = currentPage;
-//     currentPageAddOneButton.innerHTML = currentPage + 1;
-//     currentPageAddTwoButton.innerHTML = currentPage + 2;
-//     currentPageMinusOneButton.innerHTML = currentPage - 1;
-//     currentPageMinusTwoButton.innerHTML = currentPage - 2;
-//     dots1El.innerHTML = '...';
-//     dots2El.innerHTML = '...';
-//     firstPageButton.innerHTML = 1;
-//     lastPageButton.innerHTML = 20;
-//   }
-//   getMovies();
-//   createGallery();
-// });
-
-// lastPageButton.addEventListener('click', e => {
-//   e.preventDefault();
-//   currentPage = lastPage;
-//   currentPageButton.innerHTML = currentPage;
-//   if (currentPage <= 3) {
-//     currentPageButton.innerHTML = currentPage;
-//     currentPageAddOneButton.innerHTML = '';
-//     currentPageAddTwoButton.innerHTML = '';
-//     currentPageMinusOneButton.innerHTML = '';
-//     currentPageMinusTwoButton.innerHTML = '';
-//     dots1El.innerHTML = '';
-//     dots2El.innerHTML = '';
-//     firstPageButton.innerHTML = '';
-//     lastPageButton.innerHTML = '';
-//   } else {
-//     currentPageButton.innerHTML = currentPage;
-//     currentPageAddOneButton.innerHTML = currentPage + 1;
-//     currentPageAddTwoButton.innerHTML = currentPage + 2;
-//     currentPageMinusOneButton.innerHTML = currentPage - 1;
-//     currentPageMinusTwoButton.innerHTML = currentPage - 2;
-//     dots1El.innerHTML = '...';
-//     dots2El.innerHTML = '...';
-//     firstPageButton.innerHTML = 1;
-//     lastPageButton.innerHTML = 20;
-//   }
-//   getMovies();
-//   createGallery();
 // });
